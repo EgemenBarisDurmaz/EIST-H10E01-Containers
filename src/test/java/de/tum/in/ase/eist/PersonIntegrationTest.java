b04216a81d2b7b497bafac1917fec29ca2512bce
Egemen Baris Durmaz <ge85vet@mytum.de>
2023-05-02 22:39:46 +0200

    parent-child 4

--- src/test/java/de/tum/in/ase/eist/PersonIntegrationTest.java
+++ src/test/java/de/tum/in/ase/eist/PersonIntegrationTest.java
@@ -3,6 +3,7 @@ package de.tum.in.ase.eist;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import de.tum.in.ase.eist.model.Person;
 import de.tum.in.ase.eist.repository.PersonRepository;
+import de.tum.in.ase.eist.service.PersonService;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,11 +13,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.time.LocalDate;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -74,4 +75,24 @@ class PersonIntegrationTest {
     }
 
     // TODO: Add more test cases here
+    @Test
+    void testAddThreeParents() {
+        var child = new Person();
+        var parent1 = new Person();
+        var parent2 = new Person();
+        var parent3 = new Person();
+        personRepository.save(child);
+        personRepository.save(parent1);
+        personRepository.save(parent2);
+        personRepository.save(parent3);
+        PersonService ps = new PersonService(personRepository);
+        ps.addParent(child, parent1);
+        ps.addParent(child, parent2);
+        assertEquals(4, personRepository.findAll().size());
+        assertEquals(2, child.getParents().size());
+        ps.addParent(child, parent3);
+        assertThrows(ResponseStatusException.class, () -> {
+            ps.addParent(child, parent3);
+        }, "Adding more than two parents is not allowed.");
+    }
 }
